bf44a84eb62592f29aa1b457ee3225eaf2752860
lgbird <lorenzo.gebelein@gmail.com>
2025-10-26 15:08:33 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `hvac-site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag"]
+};
