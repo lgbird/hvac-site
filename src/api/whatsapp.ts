fc4705d60610edbacd4f933ed9d2c4560e7a6ef4
lgbird <lorenzo.gebelein@gmail.com>
2025-11-01 09:05:48 +0000

    edge config

--- src/api/whatsapp.ts
+++ src/api/whatsapp.ts
@@ -1,8 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export const config = {
-  runtime: 'edge',
-};
+export const runtime = 'edge';
  
 export default function handler(
   request: VercelRequest,
