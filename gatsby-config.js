72e7798d975e4e66a99499d4e9d9b697a9233e20
lgbird <lorenzo.gebelein@gmail.com>
2025-11-01 09:33:03 +0000

    google tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingIds: ['GA-TRACK']
+        trackingIds: ['AW-17670662770']
       }
     },
     {
