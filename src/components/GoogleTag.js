1d26e98836a29098096e8867d4825f0caaaabf03
lgbird <lorenzo.gebelein@gmail.com>
2025-11-01 10:16:50 +0000

    google tag

--- src/components/GoogleTag.js
+++ src/components/GoogleTag.js
@@ -1,35 +1,35 @@
-// src/components/GoogleTagManager.js
-import * as React from "react";
+import React, { useEffect } from 'react';
 import { Script } from "gatsby";
 
 const GOOGLE_TAG_ID = "AW-17670662770"; // Replace with your GTM ID
 
 export default function GoogleTagManager() {
-  return (
-<script>
+  useEffect(() => {
     function loadGoogleTag() {
-      // Prevent loading multiple times
       if (window.googleTagLoaded) return;
       window.googleTagLoaded = true;
 
-      // Dynamically create the Google Tag script element
-      var script = document.createElement('script');
-      script.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`; // Replace with your ID
+      const script = document.createElement('script');
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`;
       script.async = true;
       document.head.appendChild(script);
 
-      // Initialize dataLayer and gtag after loading script
       window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
+      function gtag(){window.dataLayer.push(arguments);}
       window.gtag = gtag;
       gtag('js', new Date());
-      gtag('config', GOOGLE_TAG_ID); // Replace with your ID
+      gtag('config', GOOGLE_TAG_ID);
 
-      // Remove the scroll event listener after loading
       document.removeEventListener('scroll', loadGoogleTag);
     }
-    document.addEventListener('scroll', loadGoogleTag);
-  </script>
-  );
-}
 
+    document.addEventListener('scroll', loadGoogleTag);
+
+    return () => {
+      // Cleanup event listener on component unmount
+      document.removeEventListener('scroll', loadGoogleTag);
+    };
+  }, []);
+
+  return null; // This component does not render any visible DOM elements
+}
