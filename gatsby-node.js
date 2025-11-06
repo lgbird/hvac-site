9cc5cc6eb521da75c33d0d65440bde391d59339a
lgbird <lorenzo.gebelein@gmail.com>
2025-11-06 13:40:22 +0000

    multiple campaigns landing pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,36 @@
 const fs = require('fs');
+const path = require('path');
 const { parse } = require('csv-parse/sync');
 
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions;
-  const csvFile = fs.readFileSync('./src/data/landing-pages.csv'); // adjust path
+  const campaignsFolder = './src/data/campaigns';
 
-  // Parse CSV to JSON-like array of objects
-  const records = parse(csvFile, {
-    columns: true, // Use first row as column headers
-    skip_empty_lines: true,
-  });
+  // Read all files in the campaigns folder
+  const csvFiles = fs.readdirSync(campaignsFolder).filter(file => file.endsWith('.csv'));
 
-  records.forEach(page => {
-    createPage({
-      path: `/${page.slug}`,
-      component: require.resolve('./src/templates/landing-page.js'),
-      context: {
-        header: page.header,
-        subheader: page.subheader,
-      },
+  // Loop over each CSV file
+  csvFiles.forEach(csvFileName => {
+    const csvFilePath = path.join(campaignsFolder, csvFileName);
+    const csvContent = fs.readFileSync(csvFilePath);
+
+    // Parse CSV file content
+    const records = parse(csvContent, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+
+    // Create a page for each record in current CSV file
+    records.forEach(page => {
+      createPage({
+        path: `/${page.slug}`, // Assuming each record has a unique slug field
+        component: require.resolve('./src/templates/landing-page.js'),
+        context: {
+          header: page.header,
+          subheader: page.subheader,
+          // Add other fields as needed
+        },
+      });
     });
   });
 };
