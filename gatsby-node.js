9a8ee512013af7b9a41f712e20ab0ab0ae4694be
lgbird <lorenzo.gebelein@gmail.com>
2025-11-06 11:03:28 +0000

    new landing pages .csv

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,24 @@
-const pages = require("./src/data/landing-pages.json");
+const fs = require('fs');
+const { parse } = require('csv-parse/sync');
 
-exports.createPages = ({ actions }) => {
+exports.createPages = async ({ actions }) => {
   const { createPage } = actions;
-  pages.forEach(page => {
+  const csvFile = fs.readFileSync('./src/data/landing-pages.csv'); // adjust path
+
+  // Parse CSV to JSON-like array of objects
+  const records = parse(csvFile, {
+    columns: true, // Use first row as column headers
+    skip_empty_lines: true,
+  });
+
+  records.forEach(page => {
     createPage({
       path: `/${page.slug}`,
-      component: require.resolve("./src/templates/landing-page.js"),
+      component: require.resolve('./src/templates/landing-page.js'),
       context: {
         header: page.header,
-        subheader: page.subheader
+        subheader: page.subheader,
       },
     });
   });
 };
-
