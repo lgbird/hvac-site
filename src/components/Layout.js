d0d7573afa304f71668259f86f8c7d7d06941443
lgbird <lorenzo.gebelein@gmail.com>
2025-11-11 08:33:38 +0000

    contentsquare

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 const Layout = ({ children }) => {
-  let initialized = useState(false);
-//<script src="https://t.contentsquare.net/uxa/82bae2a3f7f6b.js"></script>
+  const [initialized, setInitialized] = useState(false);
+  function loadContentsquareScript() {
+    if (initialized) return;
+    setInitialized(true);
+
+    const script = document.createElement('script');
+    script.src = `https://t.contentsquare.net/uxa/82bae2a3f7f6b.js`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    document.removeEventListener('scroll', loadContentsquareScript);
+  }
+  function handleWaClick(tag) {
+          fetch('/api/whatsapp', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                          "T": tag,
+                          "PARAMS": Object.fromEntries(new URLSearchParams(window.location.search).entries()),
+                          p: window.location.pathname
+                  }),
+          });
+  }
   useEffect(() => {
     fetch("/api/page", {
       method: "POST",
@@ -12,28 +33,6 @@ const Layout = ({ children }) => {
         p: window.location.pathname
       }),
     })
-    function loadContentsquareScript() {
-      if (initialized) return;
-      initialized = true;
-
-      const script = document.createElement('script');
-      script.src = `https://t.contentsquare.net/uxa/82bae2a3f7f6b.js`;
-      script.async = true;
-      document.head.appendChild(script);
-
-      document.removeEventListener('scroll', loadGoogleTag);
-    }
-    function handleWaClick(tag) {
-            fetch('/api/whatsapp', {
-                      method: 'POST',
-                      headers: { 'Content-Type': 'application/json' },
-                      body: JSON.stringify({
-                            "T": tag,
-                            "PARAMS": Object.fromEntries(new URLSearchParams(window.location.search).entries()),
-                            p: window.location.pathname
-                    }),
-            });
-    }
 
     const ctaElement = document.querySelector('.cta-float');
     ctaElement.addEventListener('click', () => handleWaClick('float'));
