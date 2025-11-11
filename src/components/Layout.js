e49687f636b77d76d91a7e1f3226a3a2f169f7d0
lgbird <lorenzo.gebelein@gmail.com>
2025-11-11 08:22:11 +0000

    contentsquare

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Layout = ({ children }) => {
+  let initialized = useState(false);
+//<script src="https://t.contentsquare.net/uxa/82bae2a3f7f6b.js"></script>
   useEffect(() => {
     fetch("/api/page", {
       method: "POST",
@@ -10,6 +12,17 @@ const Layout = ({ children }) => {
         p: window.location.pathname
       }),
     })
+    function loadContentsquareScript() {
+      if (initialized) return;
+      initialized = true;
+
+      const script = document.createElement('script');
+      script.src = `https://t.contentsquare.net/uxa/82bae2a3f7f6b.js`;
+      script.async = true;
+      document.head.appendChild(script);
+
+      document.removeEventListener('scroll', loadGoogleTag);
+    }
     function handleWaClick(tag) {
             fetch('/api/whatsapp', {
                       method: 'POST',
@@ -28,8 +41,12 @@ const Layout = ({ children }) => {
     for (const el of ctaElements) {
         el.addEventListener('click', () => handleWaClick('main'));
     }
+    document.addEventListener('scroll', loadContentsquareScript);
+    document.addEventListener('click', loadContentsquareScript);
 
     return () => {
+        document.removeEventListener('scroll', loadContentsquareScript);
+        document.removeEventListener('click', loadContentsquareScript);
         ctaElement.removeEventListener('click', () => handleWaClick('float'));
         for (const el of ctaElements) {
             el.removeEventListener('click', () => handleWaClick('main'));
