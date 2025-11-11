3dddb1962dd0a47b0458d59311eb9269a5c260a2
lgbird <lorenzo.gebelein@gmail.com>
2025-11-11 08:09:38 +0000

    contentsquare

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -18,6 +18,7 @@ const SEO = ({ title, children }) => {
 	    <meta name="description" content="Climatização Algarve. Instalação, reparação e manutenção de ar condicionado em todo o Algarve" />
 	    <meta name="google-site-verification" content="A7hePkiVawmiyF0p4EZ7Wv66gFqnXKpXn4Ife2yT5ZQ" />
 	    <link rel="icon" href="/favicon.svg" />
+	    <script src="https://t.contentsquare.net/uxa/82bae2a3f7f6b.js"></script>
 	    {children}
 	</>
     )
