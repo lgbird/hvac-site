08a760ccbe69d3c5e133366fa582388450b4f284
lgbird <lorenzo.gebelein@gmail.com>
2025-11-16 20:47:54 +0000

    washing machine specific landing

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ import { graphql, useStaticQuery } from "gatsby";
 import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import CtaBtn from '../components/CtaBtn.js';
-import imgGeneral from '../images/eletrodomesticos-r.png';
+import WashingMachineSvg from "./svg/WashingMachineSvg";
 
 const HeroSection = ({ header, subheader, campaignTag = 'GENERAL' }) => {
 	const data = useStaticQuery(graphql`
@@ -14,31 +14,24 @@ const HeroSection = ({ header, subheader, campaignTag = 'GENERAL' }) => {
         gatsbyImageData(width: 800, placeholder: BLURRED)
       }
     }
-    washingMachineImage: file(relativePath: { eq: "washing-machine.png" }) {
-      childImageSharp {
-        gatsbyImageData(width: 800, placeholder: BLURRED)
-      }
-    }
   }
 `);
-	const images = {
-		GENERAL: getImage(data.generalImage),
-		WASHING_MACHINE: getImage(data.washingMachineImage),
-	};
 
-	const image = images[campaignTag];
+	const generalImage = getImage(data.generalImage);
 
 	return (
 		<section class="hero" style={{ position: 'relative', overflow: 'hidden' }}>
 			<div class="hero-content">
 				<h1 class="hero-title">{header}</h1>
-				<GatsbyImage
-					image={image}
+				{campaignTag === 'GENERAL' ? <GatsbyImage
+					image={generalImage}
 					alt="Hero Background"
 					className="hero-img"
 					loading="eager"
 					fetchpriority="high"
 				/>
+					:
+					<WashingMachineSvg size={200} />}
 				<p class="hero-subtitle">{subheader}</p>
 				<CtaBtn />
 			</div>
