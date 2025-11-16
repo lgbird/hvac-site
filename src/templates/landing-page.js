7d5adeada85ab0ac7744b126f478f4146f0ac15c
lgbird <lorenzo.gebelein@gmail.com>
2025-11-16 14:47:31 +0000

    landing page init

--- src/templates/landing-page.js
+++ src/templates/landing-page.js
@@ -1,36 +1,76 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
-import Layout from '../components/Layout.js';
+import { SiteContext } from "../components/SiteContext.js";
+
 import SEO from '../components/Seo.js';
+import Layout from '../components/Layout.js';
 import Navbar from '../components/Navbar.js';
 import HeroSection from '../components/HeroSection.js';
-import ContactSection from '../components/ContactSection.js';
+import BelowFoldSection from "../components/BelowFoldSection.js";
+import ServicesSection from "../components/ServicesSection.js";
+import BrandsSection from "../components/BrandsSection.js";
 import ReviewsSection from '../components/ReviewsSection.js';
+import ContactSection from '../components/ContactSection.js';
 import Footer from '../components/Footer.js';
-import GoogleTag from '../components/GoogleTag.js';
+import ContactInfoSection from "../components/ContactInfoSection.js";
+
+
+const ImageRow = () => {
+  return (
+    <div class="img-row">
+      <StaticImage src="../images/wm-guy.png" />
+      <StaticImage src="../images/wm-repair.png" className="only-desktop" />
+    </div>
+  )
+}
+
+const ServiceArea = () => {
+  return (
+    <section id="area-servico" class="section">
+      <div class="img-section-container">
+        <h2 class="section-title">Area de Serviço</h2>
+        <StaticImage src="../images/area-servico.png" style={{ marginBottom: "2rem" }} imgClassName="service-area-img" className="service-area-img" />
+        <p class="section-subtitle">Vamos até si em toda Grande Lisboa!</p>
+      </div>
+    </section>
+  )
+}
+
+
+const ResponsiveRow = () => {
+  return (
+    <section class="responsive-row">
+      <ServiceArea />
+      <ContactInfoSection />
+    </section>
+  )
+}
 
 const PageTemplate = ({ pageContext }) => {
+  const { phoneNumber, formattedNumber } = useSiteConfigHook();
+  let waMessage = "XXX";
+  let waLink = `https://api.whatsapp.com/send/?phone=351${phoneNumber}&text=${encodeURIComponent(waMessage)}`;
   return (
-    <Layout>
-      <main>
-        <Navbar home={false} />
-        <HeroSection header={pageContext.header} subheader={pageContext.subheader} />
-        <SpeedAlertSection />
-        <WhyUs />
-        <ImageRow />
-        <Services />
-        <StaticImage src="../images/wm-repair.png" className="only-mobile" />
-        <BrandsSection />
-        <ReviewsSection />
-        <ResponsiveRow />
-        <ContactSection />
-        <ReviewsSection />
-        <Footer />
-        <GoogleTag />
-        <FixedBottomCta />
-      </main>
-    </Layout>
-  );
+    <SiteContext.Provider value={{ waLink, formattedNumber, phoneNumber }}>
+      <Layout>
+        <main>
+          <Navbar home={false} />
+          <HeroSection header={pageContext.header} subheader={pageContext.subheader} />
+          <SpeedAlertSection />
+          <BelowFoldSection />
+          <ImageRow />
+          <ServicesSection />
+          <StaticImage src="../images/wm-repair.png" className="only-mobile" />
+          <BrandsSection />
+          <ReviewsSection />
+          <ResponsiveRow />
+          <ContactSection />
+          <Footer />
+        </main>
+      </Layout>
+    </SiteContext.Provider>
+  )
 }
 
 export default PageTemplate;
