10d555e655c2e89d25eadcbace01626cd23fb958
lgbird <lorenzo.gebelein@gmail.com>
2025-11-17 10:48:34 +0000

    clarity tag direct injection

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,16 +31,5 @@ module.exports = {
       },
     },
     'gatsby-transformer-json',
-    {
-      resolve: `gatsby-plugin-clarity`,
-      options: {
-        // String value for your clarity project ID
-        clarity_project_id: 'u76pysinte',
-        // Boolean value for enabling clarity while developing
-        // true will enable clarity tracking code on both development and production environments
-        // false will enable clarity tracking code on production environment only
-        enable_on_dev_env: true
-      },
-    }
   ],
 };
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import CookieConsent from "react-cookie-consent";
 
 import WhatsAppBtn from '../components/whatsappBtn.js';
 import GoogleTag from '../components/GoogleTag.js';
@@ -7,6 +6,20 @@ import { SiteContext } from "./SiteContext.js";
 
 const Layout = ({ children }) => {
   const { waLink, formattedNumber } = React.useContext(SiteContext);
+  const [initialized, setInitialized] = useState(false);
+  function loadClarity() {
+    if (initialized) return;
+    setInitialized(true);
+
+    (function(c, l, a, r, i, t, y) {
+      c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
+      t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "u76pysinte");
+
+    document.removeEventListener('scroll', loadClarity);
+    document.removeEventListener('click', loadClarity);
+  }
   function handleWaClick(tag) {
     try {
       fetch('/api/whatsapp', {
@@ -42,12 +55,12 @@ const Layout = ({ children }) => {
     for (const el of ctaElements) {
       el.addEventListener('click', () => handleWaClick('main'));
     }
-    //document.addEventListener('scroll', loadClarity);
-    //document.addEventListener('click', loadClarity);
+    document.addEventListener('scroll', loadClarity);
+    document.addEventListener('click', loadClarity);
 
     return () => {
-      //document.removeEventListener('scroll', loadClarity);
-      //document.removeEventListener('click', loadClarity);
+      document.removeEventListener('scroll', loadClarity);
+      document.removeEventListener('click', loadClarity);
       ctaElement.removeEventListener('click', () => handleWaClick('float'));
       for (const el of ctaElements) {
         el.removeEventListener('click', () => handleWaClick('main'));
