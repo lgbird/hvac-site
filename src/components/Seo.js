b4f607b677ac13f73609d9370bd7df9ee716a06c
lgbird <lorenzo.gebelein@gmail.com>
2025-11-17 08:27:01 +0000

    clarity direct injection

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -11,6 +11,18 @@ const SEO = ({ titleOverride, children }) => {
 			<meta name="google-site-verification" content="A7hePkiVawmiyF0p4EZ7Wv66gFqnXKpXn4Ife2yT5ZQ" />
 			<link rel="icon" href="/favicon.svg" />
 			{children}
+
+			<script
+				async
+				type="text/javascript"
+				dangerouslySetInnerHTML={{
+					__html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "u76pysinte");`
+				}}
+			></script>
 		</>
 	)
 }
